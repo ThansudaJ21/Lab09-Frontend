4d7b019aa2805a6675fc59c958cd444adc2ba504
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-09-09 15:40:16 +0700

    5.3 Change to auto import component

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -46,12 +46,9 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
-import BaseInput from '@/components/BaseInput'
+
 export default {
   inject: ['GStore'],
-  components: {
-    BaseInput
-  },
   data() {
     return {
       event: {
